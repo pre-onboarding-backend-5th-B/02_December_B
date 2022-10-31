087041ae6047407f349545c7b60af21d0dd1e8fd
Jong012 <Jong012>
2022-10-31 17:21:08 +0900

    feat(order.models): Order 생성

--- order/models.py
+++ order/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from account.models import Account
+from company.models import StockCompany
+
+
+class Order(models.Model):
+    """
+    주문 내역
+    """
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    stock_company = models.ForeignKey(StockCompany, on_delete=models.CASCADE)
+    price = models.IntegerField(validators=[MinValueValidator(0)],
+                                default=0,
+                                help_text='주문 했을 당시 가격')
+    amount = models.IntegerField(validators=[MinValueValidator(0)],
+                                 default=0,
+                                 help_text='주문한 주식 수')
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.account.name} ordered: {self.stock_company.name}' \
+               f'({self.amount} x {self.price}) {self.ordered_at}'
