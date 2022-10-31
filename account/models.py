51e742cdb77ac97615f70b89c3f63e447ddc2a13
Jong012 <Jong012>
2022-10-31 17:12:19 +0900

    feat(account.models): Account 생성

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from company.models import StockBroker
+
+
+class Account(models.Model):
+    """
+    투자 계좌 모델
+    """
+    user = models.ForeignKey('user', on_delete=models.CASCADE)
+    number = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=32)
+    total_asset = models.IntegerField(validators=[MinValueValidator(0)],
+                                      default=0)
+    investment_principal = models.IntegerField(validators=[MinValueValidator(0)],
+                                               default=0)
+    stock_broker = models.ForeignKey(StockBroker, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.name}({self.number})'
