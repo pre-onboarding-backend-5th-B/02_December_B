d0d63cceeb39e52d21c5669b2a0d622ce68459ea
Jong012 <Jong012>
2022-11-01 18:20:00 +0900

    feat(company.models): model 수정
    1. group model 삭제
    2. StockCompany fields 수정
        - group fk -> charfield
        - price, amount delete
    3. __str__ 수정

--- company/models.py
+++ company/models.py
@@ -2,16 +2,6 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 
-class Group(models.Model):
-    """
-    투자 그룹 섹터
-    """
-    name = models.CharField(max_length=32)
-
-    def __str__(self):
-        return self.name
-
-
 class StockBroker(models.Model):
     """
     증권사
@@ -26,14 +16,9 @@ class StockCompany(models.Model):
     """
     주식 회사(투자할 회사)
     """
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    isin = models.CharField(max_length=64)
+    group = models.CharField(max_length=64)
     name = models.CharField(max_length=32)
-    price = models.IntegerField(validators=[MinValueValidator(0)],
-                                default=0,
-                                help_text='현재 주가')
-    amount = models.IntegerField(validators=[MinValueValidator(0)],
-                                 default=0,
-                                 help_text='주식 발행량 수')
 
     def __str__(self):
-        return f'{self.name}({self.group.name})'
+        return f'{self.name}/{self.isin}/{self.group}'
