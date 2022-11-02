4d496bed13cc022ed9f369fed3d6b44ec286a0f3
gyuchan <schk9611@gmail.com>
2022-11-02 22:24:54 +0900

    feat(account.serializers): investment detail 추가 작성

--- account/serializers.py
+++ account/serializers.py
@@ -10,6 +10,25 @@ class InvestmentListSerializer(serializers.ModelSerializer):
 
 
 class InvestmentDetailSerializer(serializers.ModelSerializer):
+    total_return = serializers.SerializerMethodField()
+    return_percentage = serializers.SerializerMethodField()
+
     class Meta:
         model = Account
-        fields = ["name", "stock_broker", "number", "total_asset", "investment_principal"]
+        fields = [
+            "name",
+            "stock_broker",
+            "number",
+            "total_asset",
+            "investment_principal",
+            "total_return",
+            "return_percentage",
+        ]
+
+    def get_total_return(self, obj):
+        res = obj.total_asset - obj.investment_principal
+        return res
+
+    def get_return_percentage(self, obj):
+        res = (obj.total_asset - obj.investment_principal) / (obj.investment_principal * 100)
+        return res
