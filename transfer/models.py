00ac4c19fd114d542cd86cf4b4514cbf5e02dc09
Jong012 <Jong012>
2022-11-03 00:12:23 +0900

    feat(order app): transfer 기능 완성
    1. phase 1 유효성 검사
    2. phase 2 signature 체크

---
+++ transfer/models.py
@@ -0,0 +1,30 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+
+from account.models import Account
+from company.models import StockCompany
+
+
+class Transfer(models.Model):
+    """
+    주문 내역
+    """
+    PENDING = 'P'
+    SUCCESS = 'S'
+    TRANSFER_STATUS = [
+        (PENDING, 'pending'),  # phase 1 을 통과했으나, 송금이 완료된 상태는 아님
+        (SUCCESS, 'success'),  # phase 2 까지 통과하여 송금이 성공한 상태
+    ]
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    price = models.IntegerField(validators=[MinValueValidator(0)],
+                                default=0,
+                                help_text='송금액')
+    status = models.CharField(
+        max_length=1,
+        choices=TRANSFER_STATUS,
+        help_text='송금 상태를 넣어 주세요.'
+    )
+    transfer_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.account.user_name}/{self.account.number} {self.price} {self.transfer_at}'
---
+++ transfer/views.py
@@ -0,0 +1,45 @@
+import hashlib
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from account.models import Account
+from transfer.models import Transfer
+from transfer.serializers import TransferSerializer, TransferDetailSerializer
+from transfer.utils import get_hashing
+
+
+# Create your views here.
+class TransferViewSet(viewsets.ModelViewSet):
+    queryset = Transfer.objects.all()
+    serializer_class = TransferSerializer
+
+    def create(self, request, *args, **kwargs):
+        # phase 1 을 판단
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        account = Account.objects.get(pk=request.POST.get('account'))
+        price = request.POST.get('price')
+        hashing = get_hashing(account, price)
+        transfer_id = serializer.data['id']
+        message = {
+            'signature': hashing,
+            'transfer_id': transfer_id
+        }
+        return Response(message, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        # phase 2 를 진행함 pending 에서 success 로 바꾸므로 update 라고 판단
+        request_sig = request.POST.get('signature')
+        queryset = Transfer.objects.get(pk=kwargs['pk'])
+        server_sig = get_hashing(queryset.account, queryset.price)
+        if request_sig == server_sig:
+            queryset.status = Transfer.SUCCESS
+            return Response({'status': queryset.status}, status=status.HTTP_200_OK)
+        return Response({'message': 'Does not match'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_serializer_class(self):
+        if self.action in ['retrieve', 'update']:
+            return TransferDetailSerializer
+        return TransferSerializer
