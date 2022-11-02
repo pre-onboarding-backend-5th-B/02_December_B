3773f939d59122b39a3895af203a557a5a6ee534
gyuchan <schk9611@gmail.com>
2022-11-02 13:38:20 +0900

    style(account.urls): 문서 스타일 수정

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import InvestmentAPIView
 
 urlpatterns = [
-    path('investment', InvestmentAPIView.as_view(), name='investment-list'),
+    path("investment/", InvestmentAPIView.as_view(), name="investment-list"),
 ]
